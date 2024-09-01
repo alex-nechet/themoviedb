ffa7f63e3a37cde21a9a311b0719da6189a2d45c
Alex Nechet <wildscyth@gmail.com>
2024-09-01 21:04:41 +0200

    Update impl

--- app/src/main/java/com/alex/themoviedb/ui/MainActivity.kt
+++ app/src/main/java/com/alex/themoviedb/ui/MainActivity.kt
@@ -3,20 +3,16 @@ package com.alex.themoviedb.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.alex.themoviedb.BuildConfig
 import com.alex.themoviedb.theme.TheMovieDbTheme
 import com.alex.themoviedb.ui.list.MovieListScreen
 import com.alex.themoviedb.ui.list.MovieListViewModel
 import org.koin.androidx.compose.koinViewModel
-import org.koin.compose.koinInject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TheMovieDbTheme {
                 MovieListScreen.Content(
